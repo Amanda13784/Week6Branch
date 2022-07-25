bc00b6f24dd889b54fa3b2b762eea61027f44eb3
Amanda Baker <Amanda13784@gmail.com>
2022-07-25 13:44:19 -0400

    First commit on week 6

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,20 @@ const express = require("express"),
        cors = require("cors");
 const bodyParser = require('body-parser');
 const fs = require("fs");
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
@@ -14,7 +28,7 @@ app.get("/", (req, res) => {
     });
 
 //add new item to json file
-app.post("/items", addItem)
+app.post("/items", cookieAuth, addItem)
 
 function addItem (request, response) {
     // Converting Javascript object (Task Item) to a JSON string
@@ -39,29 +53,46 @@ function addItem (request, response) {
     response.send(200)
     }
 
-app.get("/items", getItems)
+app.get("/items", cookieAuth, getItems)
 //** week5, get all items from the json database*/
   function getItems (request, response) {
     var data = fs.readFileSync('database.json');
     
     //uncomment to see the data being returned 
-    //console.log(JSON.parse(data));
+    console.log(JSON.parse(data));
 
     response.json(JSON.parse(data));
     // Note this won't work, why? response.send();
   } 
 
-app.get("/items/search",searchItems)
+app.get("/items/search", cookieAuth, searchItems)
 //**week 5, search items service */
   function searchItems (request, response) {
     var searchField = request.query.taskname;
     //uncomment to see the searchField passed in
-    //console.log(searchField);
+    console.log(searchField);
 
     var json = JSON.parse (fs.readFileSync('database.json'));
     var returnData = json.filter(jsondata => jsondata.Task === searchField);
 
     //uncomment to see the todolists found in the backend service// 
-    //console.log(returnData);
+    console.log(returnData);
     response.json(returnData);
   }
+app.get("/authenticate", auth, (req, res) => {
+  console.log(`user logging in: ${req.auth.user}`);
+  res.cookie('user', req.auth.user, { signed: true });
+  res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    const upsertSucceeded = upsertUser(username, password)
+    res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+    res.clearCookie('user');
+    res.end();
+});
